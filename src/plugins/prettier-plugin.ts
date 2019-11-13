a033f5edcda4d2b387648715789b16c300982cb1
Rob Anderson <rob@andrsn.uk>
2019-11-13 23:41:45 +0000

    :art: Move prettier to yorkie

--- src/plugins/prettier-plugin.ts
+++ src/plugins/prettier-plugin.ts
@@ -10,7 +10,7 @@ export const prettierPlugin: Plugin = {
 
     await npmPackage.addLatestDevDependencies({
       prettier: '^1.16.4',
-      husky: '^1.3.1',
+      yorkie: '^2.0.0',
       'lint-staged': '^8.1.4'
     })
 
@@ -25,10 +25,8 @@ export const prettierPlugin: Plugin = {
     //
     // Add a husky config to run lint-staged before commits
     //
-    npmPackage.addPatch('husky', PatchStrategy.persist, {
-      hooks: {
-        'pre-commit': 'lint-staged'
-      }
+    npmPackage.addPatch('gitHooks', PatchStrategy.persist, {
+      'pre-commit': 'lint-staged'
     })
 
     // Add a lint-staged config to run prettier
