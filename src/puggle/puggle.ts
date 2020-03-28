f15f9fb3e90f2966f511a1d41b9fe1cdc83f4050
Rob Anderson <rob@andrsn.uk>
2020-03-28 23:11:17 +0000

    :bug: set initial target name from the enclosing folder again

--- src/puggle/puggle.ts
+++ src/puggle/puggle.ts
@@ -1,4 +1,5 @@
 import prompts from 'prompts'
+import { basename } from 'path'
 import { Puggle, PatchStrategy } from '../types'
 import { makeConfig, loadConfig } from './config'
 import { VConfigFile, VConfigType, VDir } from '../vnodes'
@@ -13,6 +14,7 @@ export const puggle: Puggle = {
           type: 'text',
           name: 'targetName',
           message: 'project name',
+          initial: basename(targetPath === '.' ? process.cwd() : targetPath),
         },
       ],
       promptsExitProcess
